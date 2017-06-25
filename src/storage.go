b9ebd2a0e91c5b79fa98107736ed12e54cdb0ff2
angel_laptop <avictoria@MacBook-Pro-2.fios-router.home>
2017-06-24 23:09:28 -0400

    Show all records

--- src/storage.go
+++ src/storage.go
@@ -1,6 +1,8 @@
 package main
 
-func openConnection(dbType string, connectionString as string) (db *sql.DB, err error) {
-	db, err := sql.Open(dbType, connectionString)
-	return db, err 
+import "database/sql"
+
+func openConnection(dbType string, connectionString string) (db *sql.DB, err error) {
+	db, err = sql.Open(dbType, connectionString)
+	return db, err
 }
